da237f281b89ae13254e81964285dbb0a0d41bf2
Damian Brończyk <damian.bronczyk@gmail.com>
2022-08-31 23:43:29 +0200

    update error page

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -3,25 +3,42 @@
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
+
 /* @var $exception Exception */
 
 use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="blog-details-hero set-bg" data-setbg="/img/background-default.jpg"
+     style="background-image: url(/img/background-default.jpg);">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="blog__hero__text">
+                    <div class="label">Problem</div>
+                    <h2><?= Html::encode($this->title) ?></h2>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
+
+<section class="blog-details spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="blog__details__text">
+                    <h5><?= nl2br(Html::encode($message)) ?></h5>
+                    <p>
+                        The above error occurred while the Web server was processing your request.
+                    </p>
+                    <p>
+                        Please contact us if you think this is a server error. Thank you.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
